114df800d77b06e6b9ead19352ae5f35ad2a1e5a
Antonio Ballesteros <antonio.ballesteros@outlook.com>
2025-12-01 19:54:27 +0100

    optimize

--- day1.rs
+++ day1.rs
@@ -1,58 +1,44 @@
-use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::fs;
 use std::time::Instant;
 
-fn parse_direction_value(s: &str) -> Option<(char, i32)> {
-    let direction: char = s.chars().next()?;
-    let number: i32 = s[1..].parse().ok()?;
-    Some((direction, number))
+#[inline]
+fn parse_direction_value(s: &str) -> (u8, i32) {
+    let direction = s.as_bytes()[0];
+    let number = s[1..].parse().unwrap();
+    (direction, number)
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let start = Instant::now();
 
-    // part 1
-    let input_file = "day1_in.txt";
-
-    let file = File::open(input_file)?;
-    let reader = BufReader::new(file);
+    let input = fs::read_to_string("day1_in.txt")?;
 
     let mut curr_pos: i32 = 50;
     let mut count: i32 = 0;
     let mut part_2_count: i32 = 0;
 
-    for line in reader.lines() {
-        let line = line?;
-        let (dir, mut num) = parse_direction_value(&line).ok_or("Failed to parse!")?;
+    for line in input.lines() {
+        let (dir, num) = parse_direction_value(line);
 
-        if dir == 'R' {
+        if dir == b'R' {
             let new_pos = curr_pos + num;
             part_2_count += new_pos / 100;
             curr_pos = new_pos % 100;
-            if curr_pos == 0 {
-                count += 1;
-            }
+            count += (curr_pos == 0) as i32;
         } else {  // dir is L
-            while num > 0 {
-                if num > curr_pos {
-                    if curr_pos != 0 {
-                        part_2_count += 1;
-                    }
-                    num = num - curr_pos - 1;
-                    curr_pos = 99;
+            let new_pos = curr_pos - num;
+            if new_pos <= 0 {
+                part_2_count += if curr_pos != 0 {
+                    (num - curr_pos) / 100 + 1
                 } else {
-                    curr_pos = curr_pos - num;
-                    num = 0;
-                }
+                    num / 100
+                };
+                curr_pos = ((new_pos % 100) + 100) % 100;
+            } else {
+                curr_pos = new_pos;
             }
-            if curr_pos == 0 {
-                count += 1;
-                part_2_count += 1;
-            }
+            count += (curr_pos == 0) as i32;
         }
-        // println!("curr pos:     {}", curr_pos);
-        // println!("count:        {}", count);
-        // println!("part_2_count: {}", part_2_count);
     }
 
     let duration = start.elapsed();
